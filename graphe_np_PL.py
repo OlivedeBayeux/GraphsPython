60835fdc9dd5a8bcbc51c485771001760226cdfc
OlivedeBayeux <30655982+OlivedeBayeux@users.noreply.github.com>
2022-02-14 15:36:13 +0100

    Update graphe_np_PL.py
    comment

--- graphe_np_PL.py
+++ graphe_np_PL.py
@@ -18,7 +18,7 @@ class Pile:
     def clear(self):
         self.liste.clear()
 
-    # obtenir le dernier élément sans le retirer de la pile ou face
+    # obtenir le dernier élément sans le retirer de la pile
     def last(self):
         return self.liste[len(self.liste) - 1]
 
